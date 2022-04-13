b941d24ac9a2b382a5d57a885c4ce890ca0af694
Michael Bardou <michael.bardou@icloud.com>
2022-04-13 18:44:36 +0200

    started project component

---
+++ portfolio-michael/src/app/project/project-data.ts
@@ -0,0 +1,17 @@
+export const projectData = [
+  {
+    title: 'Labygame',
+    imagePath: '../../assets/Ressources/Labygame.gif',
+    description:
+      'Labygame est un projet dans fait dans un cadre scolaire avec deux camarades. Pour sortir du labyrinthe, le joueur devra trouver une clef cachée sans quoi la sortie ne sera pas ouverte. Le projet comporte trois interfaces, un menu, un environnement pour se déplacer dans le jeu et une interface de combat.',
+    tools: 'Java, Javafx',
+    githubLink: 'https://github.com/Tailong-mb/Labygame_project',
+  },
+  {
+    title: 'Labygame',
+    imagePath: '../../assets/Ressources/Labygame.gif',
+    description: 'Mettre une description',
+    tools: 'Java, javafx',
+    githubLink: 'https://github.com/Tailong-mb/Labygame_project',
+  },
+];
--- portfolio-michael/src/app/project/project.component.ts
+++ portfolio-michael/src/app/project/project.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { projectData } from './project-data';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent implements OnInit {
+  projectData = projectData;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

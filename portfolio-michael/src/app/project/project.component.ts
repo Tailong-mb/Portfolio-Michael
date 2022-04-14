210c1cd17c8ad2162c6b2b55f8aed5cebb397f3d
Michael Bardou <michael.bardou@icloud.com>
2022-04-14 14:57:07 +0200

    finished project component (add animation + data)

--- portfolio-michael/src/app/project/project.component.ts
+++ portfolio-michael/src/app/project/project.component.ts
@@ -1,10 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { projectData } from './project-data';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+  keyframes,
+  query,
+  stagger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), { optional: true }),
+
+        query(
+          ':enter',
+          stagger('300ms', [
+            animate(
+              '1s ease-in',
+              keyframes([
+                style({
+                  opacity: 0,
+                  transform: 'translateY(-75px)',
+                  offset: 0,
+                }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateY(35px)',
+                  offset: 0.3,
+                }),
+                style({ opacity: 1, transform: 'translateY(0px)', offset: 1 }),
+              ])
+            ),
+          ]),
+          { optional: true }
+        ),
+        query(
+          ':leave',
+          stagger('200ms', [
+            animate(
+              '1s ease-in',
+              keyframes([
+                style({ opacity: 1, transform: 'translateY(0px)', offset: 0 }),
+                style({
+                  opacity: 0.5,
+                  transform: 'translateY(35px)',
+                  offset: 0.3,
+                }),
+                style({
+                  opacity: 0,
+                  transform: 'translateY(-75px)',
+                  offset: 1,
+                }),
+              ])
+            ),
+          ]),
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ProjectComponent implements OnInit {
   projectData = projectData;

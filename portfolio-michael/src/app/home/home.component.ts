b431602575ba2975aa36b543d66ebef66dcd4be5
Michael Bardou <michael.bardou@icloud.com>
2022-04-14 22:14:08 +0200

    add animation home component(finished)

--- portfolio-michael/src/app/home/home.component.ts
+++ portfolio-michael/src/app/home/home.component.ts
@@ -1,9 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  transition,
+  query,
+  stagger,
+  animate,
+  style,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('cardAnimation', [
+      transition('void => *', [
+        query('.card', style({ opacity: 0, transform: 'translateX(-40px)' })),
+        query(
+          '.card',
+          stagger('500ms', [
+            animate(
+              '800ms 1.2s ease-out',
+              style({ opacity: 1, transform: 'translateX(0)' })
+            ),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor() {}

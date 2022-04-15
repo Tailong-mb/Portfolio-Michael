1a014705f22204cc392878da6b653ab6477dc742
Michael Bardou <michael.bardou@icloud.com>
2022-04-15 17:43:59 +0200

    setup dataclass for formation/exp components

--- portfolio-michael/src/app/experiences/experiences.component.ts
+++ portfolio-michael/src/app/experiences/experiences.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { dataExperience } from './data-experiences';
 
 @Component({
   selector: 'app-experiences',
   templateUrl: './experiences.component.html',
-  styleUrls: ['./experiences.component.scss']
+  styleUrls: ['./experiences.component.scss'],
 })
 export class ExperiencesComponent implements OnInit {
+  dataExperience = dataExperience;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- portfolio-michael/src/app/formations/formations.component.ts
+++ portfolio-michael/src/app/formations/formations.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { dataFormation } from './data-formations';
 @Component({
   selector: 'app-formations',
   templateUrl: './formations.component.html',
-  styleUrls: ['./formations.component.scss']
+  styleUrls: ['./formations.component.scss'],
 })
 export class FormationsComponent implements OnInit {
+  dataFormation = dataFormation;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

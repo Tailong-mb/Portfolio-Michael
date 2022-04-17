a9aca9c519432bb4e126f9e0431528a25af86d16
Michael Bardou <michael.bardou@icloud.com>
2022-04-17 19:24:24 +0200

    add data exp + formation

--- portfolio-michael/src/app/formations/data-formations.ts
+++ portfolio-michael/src/app/formations/data-formations.ts
@@ -12,7 +12,7 @@ export const dataFormation = [
     duree: '(2021 – 2022)',
     location: 'IUT CHARLEMAGNE',
     description:
-      'Apporter un soutien personnalisé aux élèves de licence afin de résoudre leurs difficultés en logique propositionnelle. L’ensemble des élèves rencontrés ont validé leurs modules de logique.',
+      "Dans le cadre d'une formation accélérée l'objectif principale était d'acquérier les bases nécessaires en informatique. Cette année m'a permis de développer un véritable attrait pour l'informatique.",
     logo: '../../assets/Ressources/computer-solid.svg',
   },
   {
@@ -20,7 +20,7 @@ export const dataFormation = [
     duree: '(2019 – 2021)',
     location: 'Faculté de lettres de NANCY',
     description:
-      'Apporter un soutien personnalisé aux élèves de licence afin de résoudre leurs difficultés en logique propositionnelle. L’ensemble des élèves rencontrés ont validé leurs modules de logique.',
+      "Le sujet de mon mémoire était \"L'impossibilité d'une méditation de la foi par l'institution religieuse chez Kierkegaard\". Cette formation a su renforcer les compétences acquises lors de la licence et d'initier à la recherche dans le domaine de la philosophie.",
     logo: '../../assets/Ressources/book-solid.svg',
   },
   {
@@ -28,15 +28,15 @@ export const dataFormation = [
     duree: '(2017 – 2019)',
     location: 'Faculté de lettres de NANCY',
     description:
-      'Apporter un soutien personnalisé aux élèves de licence afin de résoudre leurs difficultés en logique propositionnelle. L’ensemble des élèves rencontrés ont validé leurs modules de logique.',
+      "Acquisition d'une culture philosophique générale. Cette formation a permis de développer un esprit critique ainsi que des compétences relatives à l'argumentation et à l'analyse de texte.",
     logo: '../../assets/Ressources/book-solid.svg',
   },
   {
     title: 'Première année préparatoire',
     duree: '(2015 – 2016)',
-    location: 'ENSGSI',
+    location: 'ENSGSI NANCY',
     description:
-      'Apporter un soutien personnalisé aux élèves de licence afin de résoudre leurs difficultés en logique propositionnelle. L’ensemble des élèves rencontrés ont validé leurs modules de logique.',
+      "Conséquence d'une affinité pour la mathématique, j'ai intégré ce crusus préparatoire. Forcé de constater que je n'avais pas d'intéret aussi bien pour la formation que sur le métier final, j'ai quitté la formation avant son terme.",
     logo: '../../assets/Ressources/calculator-solid.svg',
   },
   {
@@ -44,7 +44,7 @@ export const dataFormation = [
     duree: '(2015)',
     location: 'Lycée Privé Saint-Léon-IX',
     description:
-      'Apporter un soutien personnalisé aux élèves de licence afin de résoudre leurs difficultés en logique propositionnelle. L’ensemble des élèves rencontrés ont validé leurs modules de logique.',
+      'J\'ai effectué une baccalauréat générale S SVT avec une spécialité mathématique. Une mention "assez bien" fut acquise. J\'ai pu développer pendant la période du lycée des méthodes de travail et une rigueur grâce à des professeurs éxigeants.',
     logo: '../../assets/Ressources/calculator-solid.svg',
   },
 ];

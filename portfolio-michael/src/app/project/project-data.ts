93cf79ecafcb6b48c467ca7f79d78b99b54e46f2
Michael Bardou <michael.bardou@icloud.com>
2022-04-18 19:36:00 +0200

    update project

--- portfolio-michael/src/app/project/project-data.ts
+++ portfolio-michael/src/app/project/project-data.ts
@@ -8,6 +8,14 @@ export const projectData = [
     githubLink: 'https://github.com/Tailong-mb/Labygame_project',
   },
   {
+    title: 'Labygame',
+    imagePath: '../../assets/Ressources/Labygame.gif',
+    description:
+      'Labygame est un projet dans fait dans un cadre scolaire avec deux camarades. Pour sortir du labyrinthe, le joueur devra trouver une clef cachée sans quoi la sortie ne sera pas ouverte. Le projet comporte trois interfaces, un menu, un environnement pour se déplacer dans le jeu et une interface de combat.',
+    tools: 'Java, Javafx, Maven, Lombok',
+    githubLink: 'https://github.com/Tailong-mb/Labygame_project',
+  },
+  {
     title: 'SnakeGame',
     imagePath: '../../assets/Ressources/SnakeGame.gif',
     description:

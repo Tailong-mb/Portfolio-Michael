20b7ea5c9b730e89546b5a27d26a4b462c7ed27f
Michael Bardou <michael.bardou@icloud.com>
2022-07-09 23:26:56 +0200

    add three dependancy + create room component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { FormationsComponent } from './formations/formations.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { SkillsComponent } from './skills/skills.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { SkillsComponent } from './skills/skills.component';
     FormationsComponent,
     ExperiencesComponent,
     SkillsComponent,
+    RoomComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/room/room.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import * as THREE from 'three';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.scss']
+})
+export class RoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

5e0352864ee93c9daa18eebeca19403970665916
Michael Bardou <michael.bardou@icloud.com>
2022-07-11 09:24:04 +0200

    continue room component

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -9,45 +9,50 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
   styleUrls: ['./room.component.scss'],
 })
 export class RoomComponent implements OnInit {
-  private url = new URL(
-    '../../assets/Ressources/bedroom.gltf',
-    import.meta.url
-  );
+  @ViewChild('canvas')
+  private canvasRef!: ElementRef;
+
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
 
   private renderer = new THREE.WebGLRenderer();
 
-  private scene = new THREE.Scene();
+  @Input() public rotationSpeedY: number = 0.007;
+
+  scene = new THREE.Scene();
 
   private camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     0.1,
-    1000
+    100
   );
 
-  private orbit = new OrbitControls(this.camera, this.renderer.domElement);
+  @Input() public orbit = new OrbitControls(
+    this.camera,
+    this.renderer.domElement
+  );
 
   private assetLoader = new GLTFLoader();
 
+  private model: THREE.Group = new THREE.Group();
+
   constructor() {}
 
-  ngOnInit(): void {
-    this.createScene();
-  }
+  ngOnInit(): void {}
 
   private createScene() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(this.renderer.domElement);
-    this.renderer.setClearColor(0xa3a3a3);
-
     this.camera.position.set(10, 10, 10);
     this.orbit.update();
 
     this.assetLoader.load(
-      this.url.href,
+      '../../assets/Ressources/bedroom.gltf',
       (gltf) => {
-        const model = gltf.scene;
-        this.scene.add(model);
+        this.model = gltf.scene;
+        this.model.rotation.y = 0;
+        this.model.rotation.x = 0;
+        this.scene.add(this.model);
       },
       undefined,
       (error) => {
@@ -55,14 +60,39 @@ export class RoomComponent implements OnInit {
       }
     );
 
-    this.renderer.setAnimationLoop(() => {
-      this.renderer.render(this.scene, this.camera);
-    });
-
     window.addEventListener('resize', () => {
       this.camera.aspect = window.innerWidth / window.innerHeight;
       this.camera.updateProjectionMatrix();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
     });
+  }
+
+  private startRenderingLoop() {
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+    this.renderer.setClearColor(0xa3a3a3);
+
+    let component: RoomComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animateModel();
+      component.renderer.render(component.scene, component.camera);
+    })();
+  }
+
+  /**
+   *Animate the model
+   *
+   * @private
+   * @memberof RoomComponent
+   */
+  private animateModel() {
+    this.model.rotation.y += this.rotationSpeedY;
+  }
+
+  ngAfterViewInit() {
+    this.createScene();
+    this.startRenderingLoop();
   }
 }

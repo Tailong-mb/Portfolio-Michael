89d276ae9e10914773d18430fa4617ef5d5070ba
Michael Bardou <michael.bardou@icloud.com>
2022-07-11 10:26:26 +0200

    continue room component

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { PerspectiveCamera } from 'three';
 
 @Component({
   selector: 'app-room',
@@ -20,31 +21,56 @@ export class RoomComponent implements OnInit {
 
   @Input() public rotationSpeedY: number = 0.007;
 
-  scene = new THREE.Scene();
+  private scene = new THREE.Scene();
 
-  private camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    100
-  );
-
-  @Input() public orbit = new OrbitControls(
-    this.camera,
-    this.renderer.domElement
-  );
+  private perspectiveCamera!: PerspectiveCamera;
 
   private assetLoader = new GLTFLoader();
 
   private model: THREE.Group = new THREE.Group();
+
+  private controls!: OrbitControls;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   private createScene() {
-    this.camera.position.set(10, 10, 10);
-    this.orbit.update();
+    this.perspectiveCamera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000.0
+    );
+
+    this.perspectiveCamera.position.setY(-5);
+
+    this.controls = new OrbitControls(
+      this.perspectiveCamera,
+      this.renderer.domElement
+    );
+    this.controls.target.set(0, 20, 0);
+    this.controls.update();
+
+    let light = new THREE.DirectionalLight(0xffffff, 1.0);
+    light.position.set(20, 100, 10);
+    light.target.position.set(0, 0, 0);
+    light.castShadow = true;
+    light.shadow.bias = -0.001;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500.0;
+    light.shadow.camera.left = 100;
+    light.shadow.camera.right = -100;
+    light.shadow.camera.top = 100;
+    light.shadow.camera.bottom = -100;
+    this.scene.add(light);
+
+    let ambientLight = new THREE.AmbientLight(0x101010);
+    this.scene.add(ambientLight);
 
     this.assetLoader.load(
       '../../assets/Ressources/bedroom.gltf',
@@ -61,8 +87,8 @@ export class RoomComponent implements OnInit {
     );
 
     window.addEventListener('resize', () => {
-      this.camera.aspect = window.innerWidth / window.innerHeight;
-      this.camera.updateProjectionMatrix();
+      this.perspectiveCamera.aspect = window.innerWidth / window.innerHeight;
+      this.perspectiveCamera.updateProjectionMatrix();
       this.renderer.setSize(window.innerWidth, window.innerHeight);
     });
   }
@@ -77,7 +103,7 @@ export class RoomComponent implements OnInit {
     (function render() {
       requestAnimationFrame(render);
       component.animateModel();
-      component.renderer.render(component.scene, component.camera);
+      component.renderer.render(component.scene, component.perspectiveCamera);
     })();
   }
 
